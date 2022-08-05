2f3deb23f1576fefaed795c37f7f3ed6f1e408c0
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-08-05 21:10:56 +0300

    task-01 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,21 @@ import styled from 'styled-components';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsInLocalStorage = JSON.parse(localStorage.getItem('contacts'));
+    if (contactsInLocalStorage) {
+      this.setState({ contacts: contactsInLocalStorage });
+    }
+  }
+
+  componentDidUpdate() {
+    const contactsInLocalStorage = JSON.stringify(this.state.contacts);
+    localStorage.setItem('contacts', contactsInLocalStorage);
+  }
 
   addContact = contact => {
     const contactsInPhonebook = this.state.contacts;
